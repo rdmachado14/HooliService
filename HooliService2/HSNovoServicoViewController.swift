dfd3dde81b6c66d2ce15d5d67adfb3c9955ad823
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-01-07 10:57:10 -0400

    Iniciando a controller de criar serviço.

---
+++ HooliService2/HSNovoServicoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HSNovoServicoViewController.swift
+//  HooliService2
+//
+//  Created by Rodrigo Machado on 1/7/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class HSNovoServicoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
