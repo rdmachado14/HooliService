cbc4ac99c82923860faa532570018fa9cd93ee9f
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-01-07 11:10:48 -0400

    Salavando titulo e descrição no Parse.

--- HooliService2/HSNovoServicoViewController.swift
+++ HooliService2/HSNovoServicoViewController.swift
@@ -7,29 +7,46 @@
 //
 
 import UIKit
+import Parse
 
-class HSNovoServicoViewController: UIViewController {
+class HSNovoServicoViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tfTitulo: UITextField!
+    @IBOutlet weak var tfDescricao: UITextField!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func btSalvar(sender: AnyObject)
+    {
+        let object = PFObject(className: "Anuncios")
+        
+        object["titulo"] = tfTitulo.text
+        object["descricao"] = tfDescricao.text
+        
+        object.saveInBackgroundWithBlock { (success, error) -> Void in
+            if success
+            {
+                print("objeto salvo!")
+            }
+            else
+            {
+                print(error)
+            }
+        }
     }
-    */
+ 
 
 }
