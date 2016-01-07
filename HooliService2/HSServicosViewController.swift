5152fbcfa60a009fcb961373a8a1819e1c46d84b
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-01-07 08:16:41 -0400

    Adicionando a classe deservicos.

---
+++ HooliService2/HSServicosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HSServicosViewController.swift
+//  HooliService2
+//
+//  Created by Rodrigo Machado on 1/7/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class HSServicosViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+
+
+}
