d2ca6973d618f5553ece088251a617bbb3d325d1
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-01-08 08:37:24 -0400

    Criando pastas e iniciando a escolha do serviço.

---
+++ HooliService2/HSTipoServicoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HSTipoServicoViewController.swift
+//  HooliService2
+//
+//  Created by Rodrigo Machado on 1/8/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class HSTipoServicoViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+
+}
